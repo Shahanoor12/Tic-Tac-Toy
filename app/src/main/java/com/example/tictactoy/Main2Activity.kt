11e3bd646de40af47275ba43448f50ba87f29115
Shahanoor12 <Shahanoor@raiseit-bd.com>
2019-10-22 11:24:59 +0600

    test2

--- app/src/main/java/com/example/tictactoy/Main2Activity.kt
+++ app/src/main/java/com/example/tictactoy/Main2Activity.kt
@@ -2,13 +2,16 @@ package com.example.tictactoy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
 
 class Main2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-        Log.d("TEST","all")
+
+        Toast.makeText(this, "test", Toast.LENGTH_LONG).show()
+
+
     }
 }
